e7be6f2cd5f870adf5c2543586a61532035bddd1
Michael Lisitsa <michael@lisitsa.dev>
2025-09-13 21:22:11 +1000

    Add zmk helper submodule - matches https://github.com/hertzquake/glove80-zmk-nix-modules/commit/9950dcdc7850356f121368e97284e3dc35214003   except not using upstream zmk fork, as the extra_modules flag is now   upstreamed in https://github.com/moergo-sc/zmk/pull/37

--- config/default.nix
+++ config/default.nix
@@ -1,11 +1,24 @@
-{ pkgs ?  import <nixpkgs> {}
-, firmware ? import ../src {}
+# We have removed the check on pkg?
+# { pkgs ?  import <nixpkgs> {}
+{ firmware ? import ../src {}
+# semi colon separated paths to modules
+, extra_modules ? ""
 }:
 
 let
   config = ./.;
 
-  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
-  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
+  glove80_left = firmware.zmk.override {
+    board = "glove80_lh";
+    keymap = "${config}/glove80.keymap";
+    kconfig = "${config}/glove80.conf";
+    extra_modules = extra_modules;
+  };
+  glove80_right = firmware.zmk.override {
+    board = "glove80_rh";
+    keymap = "${config}/glove80.keymap";
+    kconfig = "${config}/glove80.conf";
+    extra_modules = extra_modules;
+  };
 
 in firmware.combine_uf2 glove80_left glove80_right
